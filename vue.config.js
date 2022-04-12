42e167e09b678ed4844b404852fc479174c51744
LouKaiHao <1056354815@qq.com>
2022-04-12 22:01:14 +0800

    两个配置文件的配置

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// 下面是自定义配置别名
+module.exports={
+  configureWebpack:{
+    resolve:{
+      extensions:[],
+      alias:{
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views',
+      }
+    }
+  }
+}
